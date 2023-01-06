6b0397eacb7f5cc4d627add6a9f6968caa2b5ebb
Kevin Ramirez <kvnzavalza@hotmail.com>
2023-01-06 12:15:31 -0600

    - Make history object details view read only - Diff with previous object added in details view - Fix semgrep warnings

--- cl/lib/pghistory_admin.py
+++ cl/lib/pghistory_admin.py
@@ -1,13 +1,14 @@
+import pghistory.models
 from django import http
 from django.apps import apps as django_apps
-from django.conf import settings
-from django.contrib import admin, messages
+from django.contrib import admin
+from django.contrib import messages
 from django.contrib.admin import helpers
 from django.contrib.admin.utils import unquote
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import re_path, reverse
+from django.urls import re_path, reverse, resolve
 from django.utils.encoding import force_str
 from django.utils.html import mark_safe
 from django.utils.text import capfirst
@@ -20,18 +21,23 @@ class EventHistoryAdmin(admin.ModelAdmin):
     related_name_event = "event"
     order_by_history = "-pgh_id"
 
+    # def __init__(self):
+    #     super(EventHistoryAdmin, self).__init__()
+
+    def __init__(self, *args, **kwargs):
+        super(EventHistoryAdmin, self).__init__(*args, **kwargs)
+
     def _related_name_not_exist_redirect(self, request, opts, object_id):
         """Create a message informing the user that the object doesn't exist
         and return a redirect to the admin index page.
         """
         msg = _(
-            "%(name)s with related name doesn’t exist. Set correct "
-            "related_name_event in your model admin definition."
-        ) % {
-            "name": opts.verbose_name,
-        }
+            '%(name)s with related name doesn’t exist. Set correct '
+            'related_name_event in your model admin definition.') % {
+                  'name': opts.verbose_name,
+              }
         self.message_user(request, msg, messages.WARNING)
-        url = reverse("admin:index", current_app=self.admin_site.name)
+        url = reverse('admin:index', current_app=self.admin_site.name)
         return HttpResponseRedirect(url)
 
     def history_view(self, request, object_id, extra_context=None):
@@ -42,9 +48,8 @@ class EventHistoryAdmin(admin.ModelAdmin):
 
         if obj is None:
             # Object doesn't exist
-            return self._get_obj_does_not_exist_redirect(
-                request, model._meta, object_id
-            )
+            return self._get_obj_does_not_exist_redirect(request, model._meta,
+                                                         object_id)
 
         if not self.has_view_or_change_permission(request, obj):
             # User don't have permission to change object
@@ -57,21 +62,20 @@ class EventHistoryAdmin(admin.ModelAdmin):
 
         # Order all the events from object
         action_list = getattr(obj, self.related_name_event).order_by(
-            self.order_by_history
-        )
+            self.order_by_history)
 
         # Store meta from model
         opts = model._meta
 
         context = {
             **self.admin_site.each_context(request),
-            "title": _("Change history: %s") % obj,
-            "subtitle": None,
-            "action_list": action_list,
-            "module_name": str(capfirst(opts.verbose_name_plural)),
-            "object": obj,
-            "opts": opts,
-            "preserved_filters": self.get_preserved_filters(request),
+            'title': _('Change history: %s') % obj,
+            'subtitle': None,
+            'action_list': action_list,
+            'module_name': str(capfirst(opts.verbose_name_plural)),
+            'object': obj,
+            'opts': opts,
+            'preserved_filters': self.get_preserved_filters(request),
             **(extra_context or {}),
         }
 
@@ -86,7 +90,8 @@ class EventHistoryAdmin(admin.ModelAdmin):
         return render(request, template, context, **kwargs)
 
     def get_urls(self):
-        """Add additional urls to revert objects"""
+        """Add additional urls to revert objects
+        """
         urls = super().get_urls()
         admin_site = self.admin_site
         opts = self.model._meta
@@ -101,53 +106,92 @@ class EventHistoryAdmin(admin.ModelAdmin):
         return history_urls + urls
 
     def response_change(self, request, obj):
-        """Add message to indicate user if revert was successful"""
+        """Add message to indicate user if revert was successful
+        """
         verbose_name = obj._meta.verbose_name
 
         msg = _('The %(name)s "%(obj)s" was reverted successfully.') % {
             "name": force_str(verbose_name),
             "obj": force_str(obj),
         }
-        self.message_user(request, f"{msg}")
+        self.message_user(
+            request,
+            "{}".format(msg)
+        )
 
-        return http.HttpResponseRedirect(request.path)
+        opts = self.model._meta
+        info = opts.app_label, opts.model_name
+        redirect_url = "admin:%s_%s_history" % info
+
+        return http.HttpResponseRedirect(
+            reverse(redirect_url, kwargs={"object_id": obj.pk}))
 
     def response_change_failed(self, request, obj):
-        """Add message to indicate user if revert wasn't successful"""
+        """Add message to indicate user if revert wasn't successful
+        """
         verbose_name = obj._meta.verbose_name
 
         msg = _(
             'RuntimeError: The %(name)s "%(obj)s" can\'t be reverted. '
-            "Maybe some fields were excluded for tracking."
-        ) % {
-            "name": force_str(verbose_name),
-            "obj": force_str(obj),
-        }
-        self.message_user(request, f"{msg}", level=messages.ERROR)
+            'Maybe some fields were excluded for tracking.') % {
+                  "name": force_str(verbose_name),
+                  "obj": force_str(obj),
+              }
+        self.message_user(
+            request,
+            "{}".format(msg),
+            level=messages.ERROR
+        )
 
-        return http.HttpResponseRedirect(request.path)
+        opts = self.model._meta
+        info = opts.app_label, opts.model_name
+        redirect_url = "admin:%s_%s_history" % info
 
-    def history_form_view(
-        self, request, object_id, version_id, extra_context=None
-    ):
-        """View to display form with object event data, you can revert it here"""
+        return http.HttpResponseRedirect(
+            reverse(redirect_url, kwargs={"object_id": obj.pk}))
+
+    def get_readonly_fields(self, request, obj=None):
+        """Make all fields readonly
+        """
+
+        resolve_url = resolve(path=request.path)
+        if "_event_history" in resolve_url.url_name:
+            # This is revert object details view, make fields readonly
+
+            readonly_fields = list(
+                set([field.name for field in self.opts.local_fields] +
+                    [field.name for field in self.opts.local_many_to_many]))
+
+            if 'is_submitted' in readonly_fields:
+                readonly_fields.remove('is_submitted')
+
+            return readonly_fields
+        else:
+            # This is edit object view, call super for usual behavior
+            return super(EventHistoryAdmin, self).get_readonly_fields(request,
+                                                                      obj)
+
+    def history_form_view(self, request, object_id, version_id,
+                          extra_context=None):
+        """ View to display form with object event data, you can revert it here
+        """
 
         request.current_app = self.admin_site.name
         original_opts = self.model._meta
         original_model = self.model
         model = self.model._meta.get_field(
-            self.related_name_event
-        ).related_model
+            self.related_name_event).related_model
 
         # Get original object
         original_obj = get_object_or_404(
-            original_model, **{original_opts.pk.attname: object_id}
+            original_model,
+            **{original_opts.pk.attname: object_id}
         )
 
         # Get event object using version_id
         revert_obj = get_object_or_404(
             model,
-            **{original_opts.pk.attname: object_id, "pgh_id": version_id},
+            **{original_opts.pk.attname: object_id, "pgh_id": version_id}
         )
         revert_obj._state.adding = False
 
@@ -163,8 +207,8 @@ class EventHistoryAdmin(admin.ModelAdmin):
                 revert_obj.revert()
             except RuntimeError:
                 # Object can't be reverted, maybe some fields were excluded
-                return self.response_change_failed(request, revert_obj)
-            return self.response_change(request, revert_obj)
+                return self.response_change_failed(request, original_obj)
+            return self.response_change(request, original_obj)
 
         else:
             # Display form with object data, this can't be edited
@@ -179,9 +223,23 @@ class EventHistoryAdmin(admin.ModelAdmin):
             model_admin=self,
         )
 
+        pghistory_events_last_change = pghistory.models.Events.objects.filter(
+            pgh_obj_model=f"{original_opts.app_label}.{original_opts.object_name}",
+            pgh_obj_id=original_obj.pk,
+            pgh_id=revert_obj.pk).order_by(
+            "-pgh_created_at").first()
+
+        last_diff = None
+
+        if pghistory_events_last_change:
+            last_diff = pghistory_events_last_change.pgh_diff
+
         model_name = original_opts.model_name  # Original model name
 
         url_triplet = self.admin_site.name, original_opts.app_label, model_name
+        changelist_url_name = "%s:%s_%s_changelist" % url_triplet
+        change_url_name = "%s:%s_%s_change" % url_triplet
+        history_url_name = "%s:%s_%s_history" % url_triplet
         context = {
             "title": _("Revert %s") % force_str(revert_obj),
             "adminform": admin_form,
@@ -192,25 +250,19 @@ class EventHistoryAdmin(admin.ModelAdmin):
             "errors": helpers.AdminErrorList(form, formsets),
             "app_label": original_opts.app_label,
             "original_opts": original_opts,
-            "changelist_url": reverse("%s:%s_%s_changelist" % url_triplet),
-            "change_url": reverse(
-                "%s:%s_%s_change" % url_triplet, args=(original_obj.pk,)
-            ),
-            "history_url": reverse(
-                "%s:%s_%s_history" % url_triplet, args=(revert_obj.pk,)
-            ),
+            "changelist_url": changelist_url_name,
+            "change_url": change_url_name,
+            "history_url": history_url_name,
             # Context variables copied from render_change_form
             "add": False,
             "change": True,
             "has_add_permission": self.has_add_permission(request),
             # Permission on original object, to avoid add extra permissions for
             # generated event table
-            "has_change_permission": self.has_change_permission(
-                request, original_obj
-            ),
-            "has_delete_permission": self.has_delete_permission(
-                request, original_obj
-            ),
+            "has_change_permission": self.has_change_permission(request,
+                                                                original_obj),
+            "has_delete_permission": self.has_delete_permission(request,
+                                                                original_obj),
             "has_file_field": True,
             "has_absolute_url": False,
             "form_url": "",
@@ -221,6 +273,8 @@ class EventHistoryAdmin(admin.ModelAdmin):
             "save_as": self.save_as,
             "save_on_top": self.save_on_top,
             "root_path": getattr(self.admin_site, "root_path", None),
+            "last_diff": last_diff,
+            "revert_obj": revert_obj
         }
         context.update(self.admin_site.each_context(request))
         context.update(extra_context or {})
